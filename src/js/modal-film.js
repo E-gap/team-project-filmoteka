76de5ae62a7f78808fa527da53b9647e2e6c7de8
E-gap <106596181+E-gap@users.noreply.github.com>
2022-12-31 15:15:32 +0200

    1

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,22 +1,39 @@
+const refs = {
+  openModalBtn: document.querySelectorAll('[data-film-modal-open]'),
+  closeModalBtn: document.querySelector('[data-film-modal-close]'),
+  modal: document.querySelector('[data-film-modal]'),
 
-    const refs = {
-        openModalBtn: document.querySelectorAll('[data-film-modal-open]'),
-        closeModalBtn: document.querySelector('[data-film-modal-close]'),
-        modal: document.querySelector('[data-film-modal]'),
-      };
-    
-      refs.openModalBtn.forEach(e => {
-        e.addEventListener('click', toggleModal)
-      });
-      refs.closeModalBtn.addEventListener('click', toggleModal);
-    
-      function toggleModal() {
-        refs.modal.classList.toggle('is-hidden');
-      }
-    ;
-  
-  document.addEventListener('keydown', function(e) {
-      if (e.key === 'Escape') {
-      toggleModal()
-      }
-      });
+  //----------------------додав рефи------------------------------------
+  filmModal: document.querySelector('.film-modal'),
+
+  //----------------------------------------------------------
+};
+
+refs.openModalBtn.forEach(e => {
+  e.addEventListener('click', toggleModal);
+});
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    toggleModal();
+  }
+});
+
+//----------------------------додав функції та прослуховувачі--------------------------------------------------------
+
+const addToLibrary = event => {
+  if (event.target.classList.contains('add-to-watched-btn')) {
+    console.log('просмотрено');
+  }
+  if (event.target.classList.contains('add-to-queue-btn')) {
+    console.log('очередь');
+  }
+
+  console.log(event.currentTarget.getAttribute('id'));
+};
+
+refs.filmModal.addEventListener('click', addToLibrary);
